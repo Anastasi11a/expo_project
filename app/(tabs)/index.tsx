432106e9d705c586425b18d6291d652d9e5af099
Anastasiia <felitsias22@gmail.com>
2025-05-30 12:30:15 +0200

    Created an emoji picker modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,12 +6,14 @@ import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -29,7 +31,8 @@ export default function Index() {
     };
 
     const onReset = () => setShowAppOptions(false);
-    const onAddSticker = () => {};
+    const onAddSticker = () => setIsModalVisible(true);
+    const onModalClose = () => setIsModalVisible(false);
     const onSaveImageAsync = async () => {};
 
     return (
@@ -50,7 +53,9 @@ export default function Index() {
                     <Button theme='primary' label='Choose a photo' onPress={pickImageAsync} />
                     <Button label='Use this photo' onPress={() => setShowAppOptions(true)} />
                 </View>  
-            )}       
+            )}  
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+            </EmojiPicker>     
         </View>
     );
 }
