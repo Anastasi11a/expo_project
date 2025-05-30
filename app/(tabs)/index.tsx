9a4b34d0ad6df8d60f879a92ce2ea1e9ec962dbc
Anastasiia <felitsias22@gmail.com>
2025-05-30 19:13:13 +0200

    Implemented display the selected emoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -41,6 +42,9 @@ export default function Index() {
         <View style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer imgSourse={PlaceholderImage} selectedImage={selectedImage} />
+                {pickedEmoji && 
+                    <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+                }
             </View>   
             {showAppOptions ? (
                 <View style={styles.optionsContainer}>
