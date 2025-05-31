459d6ad0060c77dfc2ea0278f2ca90b6e9929c84
Anastasiia <felitsias22@gmail.com>
2025-05-31 23:01:44 +0200

    Handled platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useRef } from "react";
-import { View, ImageSourcePropType, StyleSheet } from "react-native";
+import { View, ImageSourcePropType, Platform, StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from "react-native-view-shot";
+import domtoimage from 'dom-to-image';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 
@@ -47,18 +48,34 @@ export default function Index() {
     const onModalClose = () => setIsModalVisible(false);
 
     const onSaveImageAsync = async () => {
-        try {
-            const localUri = await captureRef(imageRef, {
-                height: 440,
-                quality: 1,
-            });
+        if (Platform.OS !== 'web') {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
 
-            await MediaLibrary.saveToLibraryAsync(localUri);
-            if (localUri) {
-                alert('Saved');
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert('Saved');
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
+        } else {
+            try {
+                const dataUri = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    with: 320,
+                    height: 440,
+                });
+
+                let link = document.createElement('a');
+                link.download = 'sticker-smash.jpeg';
+                link.href = dataUri;
+            } catch (e) {
+                console.log(e);
+            }
         }
     };
 
