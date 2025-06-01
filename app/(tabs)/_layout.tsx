0722a155377493a3977a1f6fdc42843bb3831c48
Anastasiia <felitsias22@gmail.com>
2025-06-01 20:16:21 +0200

    Updated App

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -21,7 +21,11 @@ export default function TabLayout() {
                 options={{ 
                     title: 'Home' ,
                     tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                        <Ionicons 
+                            name={focused ? 'home-sharp' : 'home-outline'} 
+                            color={color} 
+                            size={24} 
+                        />
                     ),
                 }} 
             />
@@ -30,7 +34,13 @@ export default function TabLayout() {
                 options={{ 
                     title: 'About',
                     tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                        <Ionicons 
+                            name={
+                                focused ? 'information-circle' : 'information-circle-outline'
+                            } 
+                            color={color} 
+                            size={24} 
+                        />
                     ),
                 }} 
             />
